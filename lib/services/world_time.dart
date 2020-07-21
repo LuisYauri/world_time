c79abc5ddf1a716a0be6bbf9bb0b2148bc6a2c79
Luis Yauri <david.yauri147@gmail.com>
2020-07-20 20:48:33 -0500

    Add formatting showing dates

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location;
@@ -11,16 +12,22 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
+    try {
+      Response response =
+          await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
 //    print(data);
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].toString().substring(1, 3);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].toString().substring(1, 3);
 //    print(datatime);
 //    print(offset);
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    time = now.toString();
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      time = DateFormat.jm().format(now);
+    } catch (e) {
+      print('cought error: $e');
+      time = 'could not get time data';
+    }
   }
 }
